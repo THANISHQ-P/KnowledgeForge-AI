3bd7a383f0bcb52ffd45bee6e51966c7ef12b1d1
THANISHQ-P <thanishq.dev@outlook.com>
2026-07-10 09:59:00 +0530

    Update chatService.js

--- frontend/src/services/chatService.js
+++ frontend/src/services/chatService.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL || "http://localhost:5001/api/chat";
+const API = `${import.meta.env.VITE_API_URL || "http://localhost:5001/api"}/chat`;
 
 export const askQuestion = async (question) => {
-    const response = await axios.post(API, {
-        question
-    });
+  const response = await axios.post(API, {
+    question,
+  });
 
-    return response.data;
+  return response.data;
 };
